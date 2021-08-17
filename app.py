e1d25a623b36fd518aac18ef8b46771facb4cef4
oumao <evansotis2015@gmail.com>
2021-08-17 23:18:50 +0300

    removed stack routes

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask 
+from flask import Flask, redirect, url_for, render_template, flash
+from forms import ContactForm 
 
 app = Flask(__name__)
 
-@app.route("/")
+app.config['SECRET_KEY'] = b'klHy8(h27)Hjh&*&*o@8&*'
+
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
 def home():
-    return "My Name Is Ouma"
+    
+    form = ContactForm()
+
+    if form.validate_on_submit():
+        flash("Message Sent Successfully", "success")
+        return redirect('home')
+
+    return render_template('home.html', form=form)
+
+
+@app.route("/expert")
+def expert():
+    return render_template('expertise.html')
+
+
+if __name__ == '__main__':
+    app.run()
